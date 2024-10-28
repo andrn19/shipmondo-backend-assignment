a6af2745d3860deabdc6ee07871c31177ac91ac2
Andreas <andrn19@student.sdu.dk>
2024-10-28 09:57:09 +0100

    shipment controller logic; index, create, destroy

--- solution/app/controllers/shipment_controller.rb
+++ solution/app/controllers/shipment_controller.rb
@@ -1,10 +1,33 @@
 class ShipmentController < ApplicationController
   def index
+    @shipments = Shipment.all
+    render json: @shipments
   end
 
   def create
+    account = Account.first
+    shipment_data = ShipmondoService.new.create_shipment(shipment_params[:quantity_of_parcel_type])
+    @shipment = account.shipmens.new(shipment_id: shipment_data[:shipment_id])
+    @shipment.packages_attributes = shipment_data[:package_numbers].map { |pkg_no| { pkg_no: pkg_no } }
+    if @shipment.save
+      account.balance.update(amount: ShipmondoService.new.account_balance)
+      render json: @shipment.to_json(include: :packages), status: :created
+    else
+      render json: @shipment.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @shipment = Shipment.find(params[:id])
+    @shipment.destroy
+    head :no_content
   end
+
+
+  private
+
+  def shipment_params
+    params.require(:shipment).permit(:quantity_of_parcel_type)
+  end
+
 end
