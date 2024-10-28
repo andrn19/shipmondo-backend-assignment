3199007482b77e957d30e9795be56c11743a1c01
Andreas <andrn19@student.sdu.dk>
2024-10-28 09:52:17 +0100

    balance controller logic; index, show, destroy

--- solution/app/controllers/balance_controller.rb
+++ solution/app/controllers/balance_controller.rb
@@ -1,10 +1,24 @@
 class BalanceController < ApplicationController
+  before_action :set_balance, only: %i[ show destroy ]
+
   def index
+    @balances = Balance.all
+    render json: @balances
   end
 
   def show
+    render json: @balance
   end
 
   def destroy
+    @balance.destroy
+    head :no_content
   end
+
+  private
+
+  def set_balance
+    @balance = Balance.find(params[:id])
+  end
+
 end
