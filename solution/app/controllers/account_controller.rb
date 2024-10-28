f2553b5fa4a7e95f370fbea749f4570f900f5bd5
Andreas <andrn19@student.sdu.dk>
2024-10-28 09:51:16 +0100

    account controller logic; show, create

--- solution/app/controllers/account_controller.rb
+++ solution/app/controllers/account_controller.rb
@@ -1,7 +1,19 @@
 class AccountController < ApplicationController
   def show
+    account = Account.first
+    render json: account
   end
 
   def create
+    account_data = ShipmondoService.new.get_account
+    account_balance = ShipmondoService.new.get_account_balance
+    @account = Account.new(account_id: account_data[:account_id])
+    @account.balance = Balance.new(balance: account_balance[:balance])
+
+    if @account.save
+      render json: @account, status: :created
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
   end
 end
